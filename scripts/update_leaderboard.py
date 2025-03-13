a79ad79916ac28f800278e128c2364aab9b14699
Abhishek Sharma <abhisheksharma182005@gmail.com>
2025-03-13 16:28:09 +0530

    uu

--- scripts/update_leaderboard.py
+++ scripts/update_leaderboard.py
@@ -2,9 +2,9 @@ import json
 import requests
 import os
 
-GITHUB_TOKEN = ("CodeQuest Automation")  #os.getenv("CODEQUEST_ACCESS_TOKEN")
-REPO_OWNER = "your-org-or-username"
-REPO_NAME = "your-repo-name"
+GITHUB_TOKEN = ("CODEQUEST_ACCESS_TOKEN")  #os.getenv("CODEQUEST_ACCESS_TOKEN")
+REPO_OWNER = "Abhishek-Sharma182005"
+REPO_NAME = "-CodeQuest-100-Days-DSA"
 LEADERBOARD_FILE = "leaderboard.json"
 
 # Fetch participant data from the GitHub repository
