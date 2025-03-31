3cc1f892e85bee88bfad916ea4de56ad4e810d2e
Utkarsh-0003 <utkarsh.saxena_cs23@gla.ac.in>
2025-04-01 00:40:41 +0530

    my day 1 code

--- submissions/Utkarsh Saxena/first.c
+++ submissions/Utkarsh Saxena/first.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
+#include <stdio.h>
 int main(){
    printf("Hello, World!");
 
    return 0;
 
+    printf("Hello, World!");// printf is used to print the desired words we want in output as it is.
+return 0;
 }
