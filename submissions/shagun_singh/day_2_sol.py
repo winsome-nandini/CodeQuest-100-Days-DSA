841c03e536829dd498a36a44208b5fea15e8fc56
shagun111484 <shagunsingh3290@gmail.com>
2025-04-02 19:22:19 +0530

    day_2_sol.py

---
+++ submissions/shagun_singh/day_2_sol.py
@@ -0,0 +1,6 @@
+#program for printing hello ! cyberpunkcity ! and ask for user's name and greet them.
+print("Hello, Cyberpunk City! ")
+#taking input from user
+user_name=input("Enter your name : ")
+#greeting user
+print("Welcome to the adventure ",user_name)
