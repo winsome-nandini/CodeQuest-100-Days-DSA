8bebd8780e504113c5820be3aae4937242dde6d1
Tusharsharma1827 <tusharsharma27p@gmail.com>
2025-04-02 21:34:26 +0530

    DAY 2
    THANK  YOU NANDINI FOR YOUR GUIDANCE.

---
+++ submissions/TUSHAR_SHARMA/day2solution.py
@@ -0,0 +1,7 @@
+# it is used for the alphabet  
+    # now we have to print the message that is given to , for this we apply this code
+print("hello , Cyberpunk city!")
+name= input ("What is your name?")#we use input for inter  the name of the person
+print("Welcome to the adventure,",name+"!")
+
+
