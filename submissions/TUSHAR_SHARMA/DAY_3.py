e5207b176c5bab9674a624cec4ef64020c12712b
Tusharsharma1827 <tusharsharma27p@gmail.com>
2025-04-03 09:09:55 +0530

    day3.py

--- submissions/TUSHAR_SHARMA/DAY_3.py
+++
@@ -1,9 +0,0 @@
-#day 3 solution 
-#first we have to input user name ,for this we have to write this code
-name = input("Enter your name")
-#now we have write the secret code for ths we have to write this code
-Secret_code = name[:: -1].upper()
-#now we have to print the code
-print("Original Name",name)
-print("Secret code: ", Secretushart_code)
-#after following these step we get the ans
