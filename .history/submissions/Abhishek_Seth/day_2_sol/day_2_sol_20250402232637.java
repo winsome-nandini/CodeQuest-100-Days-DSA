59c4482369b0ce11de1128255fd801754b69eec0
Abhishek Sharma <abhisheksharma182005@gmail.com>
2025-04-04 13:59:02 +0530

    Delete day_2_sol_20250402232637.java

--- .history/submissions/Abhishek_Seth/day_2_sol/day_2_sol_20250402232637.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class day_2_sol {
-    public static void main(String args[]){
-        Scanner sc=new Scanner (System.in);
-        System.out.println("Hello, Cyberpunk City!\nWhat is your name?");
-        String n= sc.nextLine();
-        System.out.println("Welcome to the adventure, " + n + "!");
-    }
-}
