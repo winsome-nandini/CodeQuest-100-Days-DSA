e7aa92101c92728b907126cdb3fdf1f32909aadb
Devilwelcometohell <devangshukla218@gmail.com>
2025-04-04 17:26:55 +0530

    day4_sum_digits.py (#64)

---
+++ submissions/Devang_Shukla/day4_sum_digits.py
@@ -0,0 +1,10 @@
+# Day 4 = The Glitch's Firewall Challenge!
+
+# Taking user input
+num = input("Enter a number: ")
+
+# Calculating sum of Digits
+sum_of_digits = sum(int(digits)for digits in num)
+
+# Displaying result
+print(f"Sum of digits: {' + '.join(num)} = {sum_of_digits}")
