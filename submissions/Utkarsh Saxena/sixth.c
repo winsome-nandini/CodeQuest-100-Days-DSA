03b89b1a2ce8e0b98e7cddf5003fc2c33f6d72e3
Utkarsh-0003 <utkarsh.saxena_cs23@gla.ac.in>
2025-04-06 16:22:36 +0530

    my_day6_solution

---
+++ submissions/Utkarsh Saxena/sixth.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char choice[100];
+    printf("Which path do you choose (left/right)");//taking input from user
+    scanf("%s", choice);
+
+    if(strcmp(choice, "left")==0){//comapring input with 'left'
+        printf("You found a hidden Tunnel! You're Safe. ");
+    }else if(strcmp(choice, "right")==0){//comparing input with 'right'
+        printf("Oh no! The Glitch's trap was here! Try Again. ");
+    }else{ //case of failure
+        printf("Ivalid Choice.Please enter 'left' or 'right'");
+    }
+    return 0;
+}
