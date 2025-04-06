5ee19e74ee895b8b52edb6ac4da79da6184e81e5
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-04-06 17:23:20 +0530

    Day 6 (#91)
    * Day_5_sol
    
    * Day_sol

---
+++ submissions/DasuKoda/Day6.py
@@ -0,0 +1,7 @@
+n=str(input("Chose a Path :"))
+if(n=="left"):
+    print( "You found a hidden tunnel! You’re safe. 🚀")
+elif(n=="right"):
+    print("Oh no! The Glitch’s trap was here! Try again. 😨")
+else:
+    print( "Invalid choice. Please enter 'left' or 'right'.")
