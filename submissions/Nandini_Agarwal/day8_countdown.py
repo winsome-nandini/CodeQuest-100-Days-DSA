029aa42fe6d0636e92f1b9182ffe399a17124f8e
Nandini Agarwal <nandini10112004@gmail.com>
2025-04-08 02:33:31 +0530

    Create day8_countdown.py

---
+++ submissions/Nandini_Agarwal/day8_countdown.py
@@ -0,0 +1,4 @@
+for i in range(10,0,-1):
+#In loop, range starts from 10 as we have to print numbers from 10, then step value is -1 as we are printing descending order, then end value is 0 as it prints the value just before end value
+    print(i)
+print("Boom! Just kidding, you saved the city!")
