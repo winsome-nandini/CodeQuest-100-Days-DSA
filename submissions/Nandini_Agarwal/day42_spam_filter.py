9569fe52567854177a2a7e76baeef34a915f581c
Nandini Agarwal <nandini10112004@gmail.com>
2025-06-03 00:23:43 +0530

    Update day42_spam_filter.py

--- submissions/Nandini_Agarwal/day42_spam_filter.py
+++ submissions/Nandini_Agarwal/day42_spam_filter.py
@@ -1,7 +1,9 @@
 l1=input("Enter email subjects (comma-separated): ").split('", "')
 l2=input("Enter spam keywords (comma-separated): ").split(',')
+l1[0] = l1[0].lstrip('"')       
+l1[-1] = l1[-1].rstrip('"')     
 for i in l2:
     for j in l1:
         if i in j:
             l1.remove(j)
-print("Filtered Emails:\n","\n ".join(l1))
+print("Filtered Emails:\n-","\n- ".join(l1))
